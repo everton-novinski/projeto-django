f920baf9899290f3bd235157fb849b4731ab4749
everton novinski <evertonisa@live.com>
2022-04-20 10:26:42 -0300

    terceiro commit

--- cadastro/views.py
+++ cadastro/views.py
@@ -11,11 +11,12 @@ def home(request):
     if request.session.get('usuario'):
         usuario = Usuario.objects.get(id = request.session['usuario'])
         profissional = Profissional.objects.all()
-        return render(request, 'home.html', {'profissional': profissional})
+        return render(request, 'home.html', {'profissional': profissional, 'usuario_logado':
+                                                 request.session.get('usuario')})
     else:
         return redirect('/auth/login/?status=2') 
 
 
 def ver_profissional(request, id):
     profissional = Profissional.objects.get(id = id)
-    return render(request, 'ver_profissional.html', {'profissional' : profissional} )
+    return render(request, 'ver_profissional.html', {'profissional' : profissional, 'usuario_logado': request.session.get('usuario')} )
